e9aae5afbebee637180267b28af9eb82424312ae
rabindra789 <rabindrameher116@gmail.com>
2024-04-06 17:57:47 +0530

    practical 1

---
+++ P1_biodata.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: Rabindra Kumar Meher\n");
+    printf("DOB: 12/02/2006\n");
+    printf("Add: Bargarh, Odisha\n");
+    printf("Mob: 9821436146\n");
+    printf("Interests: Artificial Intelligence, Machine Learning, Programming\n");
+    
+    return 0;
+}
