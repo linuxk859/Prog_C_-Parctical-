4dd761f2c8a5373046806897cf89005aa9a1e3ea
rabindra789 <rabindrameher116@gmail.com>
2024-04-06 17:58:05 +0530

    Practical 2

---
+++ P2_arithmetic_operation.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a = 10, b = 5;
+    int sum, difference, multiplication, division;
+
+    // Arithmetic operations
+    sum = a + b;
+    difference = a - b;
+    multiplication = a * b;
+    division = a / b;
+
+    // Display results
+    printf("Sum: %d\n", sum);
+    printf("Difference: %d\n", difference);
+    printf("Multiplication: %d\n", multiplication);
+    printf("Division: %d\n", division);
+
+    return 0;
+}
